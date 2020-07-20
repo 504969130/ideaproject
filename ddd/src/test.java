1b101b4a7e11823b913cb8fad3aee3da1b1dc96e
leizhao <504969130@qq.com>
2020-07-20 14:35:47 +0800

    new arraylistdaasdfasgaf

--- ddd/src/test.java
+++ ddd/src/test.java
@@ -1,7 +1,7 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("sy2222sout.out");
-        System.out.println("33333.out");
+        System.out.println("44444.out");
         System.out.println("sysout.out");
     }
 }
