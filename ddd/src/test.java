fba434cbfd023c44e6d9c337bbdd0e3eb664b723
leizhao <504969130@qq.com>
2020-07-20 14:20:46 +0800

    asdf

--- ddd/src/test.java
+++ ddd/src/test.java
@@ -1,8 +1,6 @@
-import java.util.ArrayList;
-
 public class test {
     public static void main(String[] args) {
-        System.out.println("sysoasdfut.out");
-        ArrayList<String> strings = new ArrayList<String>();
+        System.out.println("sysout.out");
+        System.out.println("sysout.out");
     }
 }
