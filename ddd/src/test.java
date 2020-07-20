f9bb11251fa7b46a7b04cdf26c7835a8b69839e4
leizhao <504969130@qq.com>
2020-07-20 14:43:19 +0800

    new arraylistdaasdfasgaf

--- ddd/src/test.java
+++ ddd/src/test.java
@@ -2,7 +2,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("sy2222sout.out");
         System.out.println("5555.out");
-        System.out.println("44444.out");
+        System.out.println("444eeee44.out");
         System.out.println("sysout.out");
     }
 }
