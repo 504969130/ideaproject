26942336b8cb368d8bd74d7549f58201be2217f1
leizhao <504969130@qq.com>
2020-07-20 14:15:42 +0800

    new arraylistd

--- ddd/src/test.java
+++ ddd/src/test.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("sysout.out");
+        System.out.println("sysoasdfut.out");
         ArrayList<String> strings = new ArrayList<String>();
     }
 }
