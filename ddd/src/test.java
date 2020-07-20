b423434d8dbbc092badcb9ad97d8635cf1ea5ffc
leizhao <504969130@qq.com>
2020-07-20 14:26:53 +0800

    asdgasdf

--- ddd/src/test.java
+++ ddd/src/test.java
@@ -1,5 +1,6 @@
 public class test {
     public static void main(String[] args) {
+        System.out.println("sy2222sout.out");
         System.out.println("sy11111sout.out");
         System.out.println("sysout.out");
     }
