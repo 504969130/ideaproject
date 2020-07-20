755d3464f90df3e3218e468d93e4aa0e79550f0d
leizhao <504969130@qq.com>
2020-07-20 14:21:56 +0800

    new arraylistdaasdf

--- ddd/src/test.java
+++ ddd/src/test.java
@@ -1,6 +1,6 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("sysout.out");
+        System.out.println("sy11111sout.out");
         System.out.println("sysout.out");
     }
 }
